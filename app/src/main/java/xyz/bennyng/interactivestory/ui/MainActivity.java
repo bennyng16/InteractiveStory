eb05af1d42cb256fd411bf6ea32acaa6f3e229f9
Benny Ng <bennyng@umich.edu>
2015-10-26 11:13:21 -0400

    Interactive Story
    Working through Treehouse.com's Android App class

---
+++ app/src/main/java/xyz/bennyng/interactivestory/ui/MainActivity.java
@@ -0,0 +1,49 @@
+package xyz.bennyng.interactivestory.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import xyz.bennyng.interactivestory.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameField;
+    private Button mStartButton;
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameField = (EditText)findViewById(R.id.nameEditText);
+        mStartButton = (Button)findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
+    }
+
+    @Override
+    protect ed void onResume() {
+        super.onResume();
+        // Reset name field
+        // mNameField.setText("");
+    }
+}
